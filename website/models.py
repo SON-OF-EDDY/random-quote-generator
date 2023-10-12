0dae0c2127bf37ab35f67059c560d06b70f15bbe
tim <timohide13@gmail.com>
2023-10-12 10:32:21 +0300

    final commit...

--- website/models.py
+++ website/models.py
@@ -2,8 +2,3 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-import uuid
-
-class Image(models.Model):
-    unique_identifier = models.UUIDField(default=uuid.uuid4, unique=True)
-    image_data = models.BinaryField()
