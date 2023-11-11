eb182ee6d67f4a8a5f2f9b558b814873a4922688
tim <timohide13@gmail.com>
2023-11-11 21:51:58 +0300

    fixing...

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,10 +13,10 @@ SECRET_KEY = 'django-insecure-zxejxwm9zfybbb_dr9r24uz@qxxa&%53(aqc3(j^24#ai871uc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ["https://web-production-245f.up.railway.app","web-production-245f.up.railway.app","hyde-api.win","www.hyde-api.win"]
-CSRF_TRUSTED_ORIGINS = ["https://web-production-245f.up.railway.app","web-production-245f.up.railway.app","hyde-api.win","www.hyde-api.win"]
-
+ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ["https://web-production-245f.up.railway.app","web-production-245f.up.railway.app","hyde-api.win","www.hyde-api.win"]
+#CSRF_TRUSTED_ORIGINS = ["https://web-production-245f.up.railway.app","web-production-245f.up.railway.app","hyde-api.win","www.hyde-api.win"]
+CSRF_TRUSTED_ORIGINS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
